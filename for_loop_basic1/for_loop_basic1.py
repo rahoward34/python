46b984fe1aee9dc88ff77cce9e4f8f01cde86b2c
Ross Howard <116023080+rahoward34@users.noreply.github.com>
2023-03-15 13:57:41 -0400

    Add files via upload
    These are my projects for the fundamentals of Python portion of my Bootcamp.

---
+++ for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,30 @@
+for x in range(0,101):
+    print(x)
+
+for x in range(5,1001,5):
+    print(x)
+
+for x in range(1,101):
+    i = "coding"
+    y = "Coding Dojo"
+    if x % 5 == 0:
+            print(i)
+    if x % 10 == 0:
+            print(y)
+    else:
+        print(x)
+
+sum = 0 
+for i in range(1,50000,2):
+    sum += i 
+print(sum)
+
+for i in range(2018,0,-4):
+    print(i)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(2,16):
+    if x % 3 ==0:
+        print(x)
